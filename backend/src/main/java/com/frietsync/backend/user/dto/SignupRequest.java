fb1be97f0d65e71bcddecac3038c6db729166c8c
Avi Singh <aviarya2006@gmail.com>
2026-09-25 17:38:53 +0530

    feat:Add incoming request body

---
+++ backend/src/main/java/com/frietsync/backend/user/dto/SignupRequest.java
@@ -0,0 +1,26 @@
+
+package com.frietsync.backend.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    private String password;
+}
