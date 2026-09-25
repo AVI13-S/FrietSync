2b2c382bb979e37a7fe9dd7a3d2b6ffa31183178
Avi Singh <aviarya2006@gmail.com>
2026-09-25 17:01:46 +0530

    fix :changed Long to UUID

--- backend/src/main/java/com/frietsync/backend/common/entity/BaseEntity.java
+++ backend/src/main/java/com/frietsync/backend/common/entity/BaseEntity.java
@@ -13,6 +13,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @Data
 @MappedSuperclass
@@ -21,7 +22,7 @@ public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
