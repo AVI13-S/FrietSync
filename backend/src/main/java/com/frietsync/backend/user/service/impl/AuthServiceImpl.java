cdee42c507b8714c79ba6b7b4202553bd8e01eec
aviralumrao <aviral0419@gmail.com>
2026-09-25 22:10:10 +0530

    feat : added login

--- backend/src/main/java/com/frietsync/backend/user/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/frietsync/backend/user/service/impl/AuthServiceImpl.java
@@ -1,12 +1,13 @@
 package com.frietsync.backend.user.service.impl;
 
 import com.frietsync.backend.common.exception.BadRequestException;
+import com.frietsync.backend.user.dto.LoginRequest;
 import com.frietsync.backend.user.dto.SignupRequest;
 import com.frietsync.backend.user.dto.UserResponse;
-import com.frietsync.backend.user.service.AuthService;
 import com.frietsync.backend.user.entity.User;
 import com.frietsync.backend.user.enums.Role;
 import com.frietsync.backend.user.repository.UserRepository;
+import com.frietsync.backend.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,6 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
 
     public UserResponse signup(SignupRequest request) {
         if (userRepository.existsByEmail(request.getEmail())) {
@@ -30,12 +30,23 @@ public class AuthServiceImpl implements AuthService {
                 .name(request.getName())
                 .email(request.getEmail())
                 .passwordHash(hashedPassword)
-                .active(true)
                 .role(Role.ADMIN)
+                .active(true)
                 .build();
 
         User savedUser = userRepository.save(user);
 
         return UserResponse.fromEntity(savedUser);
+    }
+
+    public UserResponse login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new BadRequestException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())) {
+            throw new BadRequestException("Invalid email or password");
+        }
+
+        return UserResponse.fromEntity(user);
     }
 }
