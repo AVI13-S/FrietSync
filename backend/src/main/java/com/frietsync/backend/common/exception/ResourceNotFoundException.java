a364e7e65101810d34d03214bb8dd00c5bb71a5f
aviralumrao <aviral0419@gmail.com>
2026-09-25 08:09:08 +0530

    feat: add bassic exception handler

---
+++ backend/src/main/java/com/frietsync/backend/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.frietsync.backend.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
